b513975512d5846781426870d34a9b555feccf89
atommnava <atommunoznava@gmail.com>
2025-05-24 22:41:11 -0600

    Iterating over a linked list in C

--- iterating_linklist.c
+++ iterating_linklist.c
@@ -6,13 +6,20 @@ int main(void)
     root.x = 15;
     root.next = malloc(sizeof(Node));
     root.next -> x = -2;
-    root.next -> next = NULL;
+    root.next -> next = malloc(sizeof(Node));
+    root.next -> next -> x = 22;
+    root.next -> next -> next = NULL;
 
     Node *curr = &root;
     while (curr != NULL) {
         printf("%d\n", curr -> x);
         curr = curr -> next;
     }
-    
+    /*  EQUIVALENT, NOT RECOMENDED! :)
+        for (Node *curr = &root; curr != NULL; curr -> curr -> next) {
+            printf("%d\n", curr -> x);
+        }
+    */
+    free(root.next -> next);
     free(root.next);
 }
