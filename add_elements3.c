cb027e12062a63d8f905bfcecc63fda79a3e8c9f
atommnava <atommunoznava@gmail.com>
2025-05-28 20:53:42 -0600

    Add after an element in a linked list

---
+++ add_elements3.c
@@ -0,0 +1,64 @@
+#include "my-structure.h"
+
+void insertEnd(Node **root, int value)
+{
+    Node *newNode = malloc(sizeof(Node));
+    Node *curr = *root;
+    if (newNode == NULL) exit(1);
+    newNode -> next = NULL;
+    newNode -> x = value; 
+
+    if (*root == NULL) {
+        *root = newNode;
+        return;
+    }
+
+    while (curr -> next != NULL) {
+        curr = curr -> next;
+    }
+    curr -> next = newNode;
+}
+
+void insertBeggining(Node **root, int value)
+{
+    Node *newNode = malloc(sizeof(Node));
+    if (newNode == NULL) exit(3);
+    newNode -> x = value;
+    newNode -> next = *root;
+    *root = newNode;
+}
+
+void inserAfter(Node *node, int value)
+{
+    Node *newNode = malloc(sizeof(Node));
+    if (newNode == NULL) {
+        exit(4);
+    }
+    newNode -> x = value;
+    newNode -> next = node -> next;
+    node -> next = newNode;
+}
+int main(void)
+{
+    Node *root = NULL;
+    int first, second, input_valid;
+    printf("1st integer: ");
+    scanf("%d", &first);
+
+    printf("2snd integer: ");
+    scanf("%d", &second);
+
+    insertEnd(&root, first);
+    insertEnd(&root, second);
+    insertEnd(&root, 5);
+    inserAfter(root, 7);
+
+    
+    for (Node *curr = root; curr != NULL; curr = curr -> next) {
+        printf("%d\n", curr->x);
+    }
+    
+    free(root);
+
+    return 0;
+}
