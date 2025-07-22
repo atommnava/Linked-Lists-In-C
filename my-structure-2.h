5171e0c50feefb13aa6f1d887222b166280d7683
atommnava <atommunoznava@gmail.com>
2025-07-22 15:07:06 -0600

    Structure 2

---
+++ my-structure-2.h
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+typedef struct Node {
+    int x;
+    struct Node* next;
+    struct Node* prev;
+} Node;
+
+void insertBeggining(Node **root, int value);
+void inserAfter(Node *node, int value);
+void insertEnd(Node **root, int value);
+void deallocate(Node **root);
+void insertSorted(Node **root, int value);
+void removeElement(Node **root, int value);
