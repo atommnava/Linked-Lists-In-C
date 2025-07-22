c0af310f8e42fa431cc2674ce3cf72aabebc85d9
atommnava <atommunoznava@gmail.com>
2025-07-22 14:58:57 -0600

    REversing a linked list

---
+++ reversing_list.c
@@ -0,0 +1,124 @@
+#include "my-structure.h"
+
+void insertEnd(Node **root, int value)
+{
+    Node *newNode = malloc(sizeof(Node));
+    Node *curr = *root;
+    if (newNode == NULL) exit(1);
+    newNode -> next = NULL;
+    newNode -> x = value; 
+
+    if (*root == NULL) {
+        *root = newNode;
+        return;
+    }
+
+    while (curr -> next != NULL) {
+        curr = curr -> next;
+    }
+    curr -> next = newNode;
+}
+
+void insertBeggining(Node **root, int value)
+{
+    Node *newNode = malloc(sizeof(Node));
+    if (newNode == NULL) exit(3);
+    newNode -> x = value;
+    newNode -> next = *root;
+    *root = newNode;
+}
+
+void inserAfter(Node *node, int value)
+{
+    Node *newNode = malloc(sizeof(Node));
+    if (newNode == NULL) {
+        exit(4);
+    }
+    newNode -> x = value;
+    newNode -> next = node -> next;
+    node -> next = newNode;
+}
+
+void insertSorted(Node **root, int value)
+{
+  //if (*root == NULL || (*root) -> x <= value) EQUIVALENT
+  //                        2           1
+  // 1 -> 2 -> 3 -> 4
+    if (*root == NULL || (**root).x >= value ) {
+        insertBeggining(root, value);
+        return;
+        // ONLY break; IT IS EQUIVALENT
+    }
+    Node *curr = *root;
+    while (curr -> next != NULL) {
+        if (curr -> next -> x >= value) {
+            inserAfter(curr, value);
+            return;
+        }
+        curr = curr -> next;
+    }
+    inserAfter(curr, value);
+}
+
+void removeElement(Node **root, int value)
+{
+    Node *curr = *root;
+    Node *to_remove = NULL;
+    if (*root == NULL) {
+        return;
+    }
+
+    if ((*root) -> x == value) {
+        to_remove = *root;
+        *root = (*root) -> next;
+        free(to_remove); 
+        return;
+    }
+
+    while (curr -> next != NULL) {
+        if (curr -> next -> x == value) {
+            to_remove = curr -> next;
+            curr -> next = curr -> next -> next;
+            free(to_remove);
+            return;
+        }
+        curr = curr -> next;
+    }
+}
+
+
+int main(void)
+{
+    system("clear");
+    Node *root = NULL;
+    int first, second, third, fourth;
+    printf("1st integer: ");
+    scanf("%d", &first);
+
+    printf("2nd integer: ");
+    scanf("%d", &second);
+
+    printf("3rd integer: ");
+    scanf("%d", &third);
+
+    printf("4th integer: ");
+    scanf("%d", &fourth);
+
+    insertSorted(&root, first);
+    insertSorted(&root, second);
+    insertSorted(&root, third);
+    insertSorted(&root, fourth);
+    removeElement(&root, first);
+    Node *curr = root;
+    while(curr -> next != NULL) {
+        curr = curr -> next;
+    }
+    /*
+    for (Node *curr = root; curr != NULL; curr = curr -> next) {
+        printf("%d\n", curr->x);
+    }
+    */
+    free(root);
+
+    return 0;
+}
