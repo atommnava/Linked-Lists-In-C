a1f0b57af0597b63e4711732d223a119877495d4
atommnava <atommunoznava@gmail.com>
2025-07-23 16:39:29 -0600

    Introduction to doubly linked lists

--- double_linked_list.c
+++ double_linked_list.c
@@ -8,5 +8,17 @@ int main(void)
     }
     tail -> x = 1;
     tail -> prev = NULL;
+    tail -> next = malloc(sizeof(Node));
+    if (tail -> next == NULL) {
+        return 2;
+    }
+    tail -> next -> x = 3;
+    tail -> next -> prev = tail;
+    tail -> next -> next = malloc(sizeof(Node));
+    if (tail -> next -> next == NULL) {
+        return 3;
+    }
+    tail -> next -> next -> x = 7;
+    tail -> next -> next -> prev = tail;
     return 0;
 }
