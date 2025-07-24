13de737e9ed5eb1382ab2f1504d0ec3a5c00601c
atommnava <atommunoznava@gmail.com>
2025-07-24 14:50:24 -0600

    Iterating over doubly linked lists

---
+++ double_linked_list_p1.c
@@ -0,0 +1,27 @@
+#include "my-structure-2.h"
+
+int main(void)
+{
+    Node *tail = malloc(sizeof(Node));
+    if (tail == NULL) {
+        return 1;
+    }
+    tail -> x = 1;
+    tail -> prev = NULL;
+    tail -> next = malloc(sizeof(Node));
+    if (tail -> next == NULL) {
+        return 2;
+    }
+    tail -> next -> x = 3;
+    tail -> next -> prev = tail;
+    tail -> next -> next = malloc(sizeof(Node));
+    if (tail -> next -> next == NULL) {
+        return 3;
+    }
+    tail -> next -> next -> x = 7;
+    tail -> next -> next -> prev = tail;
+    tail -> next -> next -> next = NULL;
+    Node *head = tail -> next -> next;
+
+    return 0;
+}
