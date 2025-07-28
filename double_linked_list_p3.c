efb0882f345a837eba899e900ec2188077c29c15
atommnava <atommunoznava@gmail.com>
2025-07-28 11:27:13 -0600

    Add to the beginning of a doubly linked list

---
+++ double_linked_list_p3.c
@@ -0,0 +1,53 @@
+#include "my-structure-2.h"
+
+void deallocate(Node **tail, Node **head)
+{
+    if (*tail == NULL) {
+        return;
+    }
+    Node *curr = *tail;
+    while (curr -> next != NULL) {
+        curr = curr -> next;
+        free(curr -> prev);
+    }
+    free(curr);
+    *tail = NULL;
+    *head = NULL;
+}
+
+int main(void)
+{
+    Node *tail = malloc(sizeof(Node));
+    if (tail == NULL) {
+        return 1;
+    }
+    tail -> x = 1;
+    tail -> prev = NULL;
+    tail -> next = malloc(sizeof(Node));
+    if (tail -> next == NULL) {
+        return 2;
+    }
+    tail -> next -> x = 3;
+    tail -> next -> prev = tail;
+    tail -> next -> next = malloc(sizeof(Node));
+    if (tail -> next -> next == NULL) {
+        return 3;
+    }
+    tail -> next -> next -> x = 7;
+    tail -> next -> next -> prev = tail -> next;
+    tail -> next -> next -> next = NULL;
+    Node *head = tail -> next -> next;
+
+    Node *curr = tail;
+    while (curr != NULL) {
+        printf("Value: %d\n", curr -> x);
+        curr = curr -> next;
+    }
+    deallocate(&tail, &head);
+    /*
+    for (Node *curr = tail; curr != NULL; curr = curr -> next) {
+        printf("Value: %d\n", curr -> x);
+    }
+    */
+    return 0;
+}
